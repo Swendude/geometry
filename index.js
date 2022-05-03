ee2f577e9338d8eb4d124a38c9f66edd19638842
Swen Mulderij <swenmulderij@gmail.com>
2022-05-03 11:03:28 +0200

    feat/added_rect_area

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function area(w, h) {
   return w * h;
 }
+function squareArea(w) {
+  return w * w;
+}
 
 function circumference(w, h) {
   return 2 * w + 2 * h;
@@ -10,4 +13,4 @@ function diagonal(w, h) {
   return Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2));
 }
 
-module.exports = { area, circumference, diagonal };
+module.exports = { area, circumference, diagonal, squareArea };
