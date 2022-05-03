13ddfb0ab94528e7b3634011046a22de2ceb9c4c
Swen Mulderij <swenmulderij@gmail.com>
2022-05-03 11:02:30 +0200

    feat/added_circle_area

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function area(w, h) {
   return w * h;
 }
 
+function circleArea(r) {
+  return 2 * Math.PI * Math.pow(r, 2);
+}
+
 function circumference(w, h) {
   return 2 * w + 2 * h;
 }
@@ -10,4 +14,4 @@ function diagonal(w, h) {
   return Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2));
 }
 
-module.exports = { area, circumference, diagonal };
+module.exports = { area, circumference, diagonal, circleArea };
